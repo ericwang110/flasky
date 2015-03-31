17d7c540f1747206a11ffb8cc6a19a656fdd5dcf
ericwang110 <1060339962@qq.com>
2015-03-31 17:13:21 +0800

    chapter2
    示例2-3

--- hello.py
+++ hello.py
@@ -1,6 +1,8 @@
 
+from flask.ext.script import Manager
 from flask import Flask
 app = Flask(__name__)
+manager = Manager(app)
 
 
 @app.route('/')
@@ -15,3 +17,4 @@ def user(name):
 
 if __name__ == '__main__':
     app.run(debug=True)
+    manager.run()
